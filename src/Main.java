21b27020749e2791b76d43acc5db3088221248cf
scottcapa <scottcapacordova@gmail.com>
2026-01-17 21:41:24 -0500

    peticion de datos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello and welcome!");}
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in)
+
+        System.out.println("Elija una opcion");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+
+        int opcion = teclado.nextInt();
+    }
 }
