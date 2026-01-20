e2ada2079ea00eb788dc6e4b2cd8fc976869754f
xavier <xavierpardo876@gmail.com>
2026-01-19 19:16:29 -0500

    peticion de datos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        /*XAVIER*/
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Bienvenidos al Sistema :)");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Ingrese el primer numero");
+        int num1 = teclado.nextInt();
+
+        System.out.printf("Ingrese el primer numero");
+        int num2 = teclado.nextInt();
 
 
 
